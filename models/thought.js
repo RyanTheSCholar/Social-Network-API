274988dae49d1039ce17b338fe9d075411fe8843
RyanTheScholar <Ryan.business.bowen@gmail.com>
2023-08-17 02:28:26 -0500

    added all the models

---
+++ models/thought.js
@@ -0,0 +1,38 @@
+const { Schema, model } = require("mongoose");
+const Reaction = require("./reaction");
+
+const thoughtSchema = new Schema(
+  {
+    thoughtText: {
+      type: String,
+      required: true,
+      minlength: 1,
+      maxlength: 280,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      // add some get property to change the date to something readable
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    reactions: [
+      {
+        reactions:[Reaction],
+      },
+    ],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+      getters: true,
+    },
+    id: false,
+  }
+);
+
+const Thought = model("thought", thoughtSchema);
+
+module.exports = Thought;
---
+++ models/user.js
@@ -0,0 +1,43 @@
+const { Schema, model } = require("mongoose");
+const validateEmail = function (email) {
+  const regex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+  return regex.test(email);
+};
+
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      validate: [validateEmail, "Please enter a valid email"],
+    },
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Thought",
+      },
+    ],
+    friends: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  }
+);
+const User = model("user", userSchema);
+
+module.exports = User;
